283a2cd54cb7f6de6da52170037992777cbc8709
Wilson Cheng <wilson.system1@gmail.com>
2012-12-20 11:44:50 +0800

    Step-8b

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,8 +11,10 @@ class StaticPagesController < ApplicationController
   end
 
   def home
-    @micropost = current_user.microposts.build if signed_in?
+    if signed_in?
+    @micropost = current_user.microposts.build
     @feed_items = current_user.feed.paginate(page: params[:page])
+    end 
   end 
 
 
